805f99b4dd314b8ba8ce93ed2a0d1d913439448c
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-09-21 13:07:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+
+
+function returnCustomerName() {
+  return customerName;
+}
+
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer = 'old least favorite customer';
+   leastFavoriteCustomer = 'new least favorite customer'; 
+}
+
